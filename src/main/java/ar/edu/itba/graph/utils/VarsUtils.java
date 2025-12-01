ae534847facabe36fa846582fb0ad5f6bba7e98b
santiagohirsch <shirsch@itba.edu.ar>
2025-12-01 18:34:28 -0300

    refactor: nodes instead of vertices

--- src/main/java/ar/edu/itba/graph/utils/VarsUtils.java
+++ src/main/java/ar/edu/itba/graph/utils/VarsUtils.java
@@ -13,12 +13,12 @@ public class VarsUtils {
     public static final String GRAPHFRAMES_SRC_ALIAS = "src_id";
     public static final String GRAPHFRAMES_DST_ALIAS = "dst_id";
     public static final String GRAPHFRAMES_NEIGHBOR_COL = "neighbor";
-    public static final String VERTICES_EXTENSION = "-vertices.csv";
+    public static final String VERTICES_EXTENSION = "-nodes.csv";
     public static final String EDGES_EXTENSION = "_edges.csv";
     public static final String COUNT = "count";
     public static final String INNER = "inner";
     public static final String EMPTY_K_CORE_MESSAGE = "The k-core decomposition resulted in an empty graph.";
     public static final String K_CORE_COMPUTED_MESSAGE = "K-core decomposition computed successfully.";
-    public static final String VERTICES_MESSAGE = "Vertices: ";
+    public static final String VERTICES_MESSAGE = "Nodes: ";
     public static final String EDGES_MESSAGE = "Edges: ";
 }
